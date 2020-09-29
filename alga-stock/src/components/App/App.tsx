a732260df1ae219e32c0c1df0ed83ca6ae7a7262
dvsilva <dvsilva@gmail.com>
2020-09-29 09:57:30 -0300

    add header component

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "../Header";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header title="AlgaStock" />
+    </div>
+  );
 }
 
 export default App;
