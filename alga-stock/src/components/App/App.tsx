0671778b5f3153efaf7d041f30580f4fa145df03
dvsilva <dvsilva@gmail.com>
2020-10-01 06:36:14 -0300

    clear app + add table component's base files

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -1,49 +1,14 @@
-import React, { useState } from "react";
-import Button from "../../shared/Button";
+import React from "react";
 import Container from "../../shared/Container";
-import Input from "../../shared/Input";
 import Header from "../Header";
 import "./App.css";
 
-function TestComponent() {
-  return (
-    <img
-      width="16"
-      src="https://img.icons8.com/pastel-glyph/2x/search--v2.png"
-      alt="search icon"
-    />
-  );
-}
-
 function App() {
-  const [street, setStreet] = useState("");
-
   return (
     <div className="App">
       <Header title="AlgaStock" />
 
-      <Container>
-        <ul>
-          {["Daniel", "Thiago", "William", "Daniel"].map((name, index) => {
-            return <li key={index}>{name}</li>;
-          })}
-        </ul>
-
-        <Button content="Click me" onClick={() => window.alert("alert")} />
-        <Button onClick={() => window.alert("alert button 1")}>Alert</Button>
-        <Button
-          onClick={() => window.alert("alert button 2")}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-        <Input
-          label="Street"
-          placeholder="E.g.: 15th avenue"
-          value={street}
-          onChange={(e) => setStreet(e.target.value)}
-        />
-      </Container>
+      <Container></Container>
     </div>
   );
 }
