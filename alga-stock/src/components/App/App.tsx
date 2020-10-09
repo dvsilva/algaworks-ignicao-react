0d63331c0553a7baf73d621d5b835369016b7cff
dvsilva <dvsilva@gmail.com>
2020-10-09 07:12:14 -0300

    add form component

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Button from "../../shared/Button";
 import Container from "../../shared/Container";
+import Form from "../../shared/Form/Form";
+import Input from "../../shared/Input";
 import Table, { TableHeader } from "../../shared/Table";
 import Products from "../../shared/Table/Table.mockdata";
 import Header from "../Header";
@@ -19,6 +22,19 @@ function App() {
 
       <Container>
         <Table data={Products} headers={headers} />
+
+        <Form title="Product form" onSubmit={console.log}>
+          <Input label="Name" placeholder="E.g.: Cookie" />
+          <Input
+            label="Price"
+            type="number"
+            step="0.01"
+            min="0"
+            placeholder="E.g.: 1.25"
+          />
+          <Input label="Stock" type="number" min="0" placeholder="E.g.: 15" />
+          <Button>Submit</Button>
+        </Form>
       </Container>
     </div>
   );
