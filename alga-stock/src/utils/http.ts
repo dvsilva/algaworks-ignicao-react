73721503761204d6c090950735fe804b697d3695
dvsilva <dvsilva@gmail.com>
2020-11-19 08:19:36 -0300

    config http Bearer token

--- alga-stock/src/utils/http.ts
+++ alga-stock/src/utils/http.ts
@@ -1,13 +1,24 @@
 import axios from 'axios';
+import {store} from '../redux';
 
 // const http = axios.create({baseURL: 'https://jsonplaceholder.typicode.com'});
 // const http = axios.create({baseURL: 'http://localhost:3024'});
+
 const http = axios.create({
   // baseURL: 'http://localhost:4010', // mockserver
   baseURL: 'http://localhost:3024', // API local 
-  headers: {
-    authorization: 'Bearer 123'
-  }
+  // headers: {
+  //   authorization: 'Bearer 123'
+  // }
 });
+
+http.interceptors.request.use((config) => {
+  const token = store.getState().authentication.profile?.token;
+  
+  if(token)
+    config.headers.Authorization = `Bearer ${token}`
+  
+    return config;
+})
 
 export default http;
